dffb84dbd23235538d68b421ac075527d1232642
Franco Trimboli <sunpazed@gmail.com>
2025-03-12 11:13:08 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## OpenAI Agents SDK demo
+
+Really simple demo of the (OpenAI Agents SDK)[https://platform.openai.com/docs/guides/agents]
+
+```
+pip install openai-agents
+```
+
+```
+python agent_multi_weather.py
+```
+
+Query;
+
+```
+Which city is currently the coldest? Melbourne, Chicago, Oslo, Dublin, or Shanghai? Will I need an umbrella in this city in the next few days?
+```
+
+Response;
+
+```
+The coldest city currently is Oslo, with a temperature of 0.2°C.
+
+For the next few days in Oslo, there's no significant precipitation expected, so you likely won't need an umbrella.
+```
+
---
+++ agent_multi_weather.py
@@ -0,0 +1,38 @@
+from agents import Agent, Runner, function_tool
+import requests
+import json
+
+@function_tool
+def get_weather_tool(latitude: float, longitude: float):
+    """
+    Retrieves weather data from the Open-Meteo API.
+
+    Args:
+        latitude (number): The latitude of the location.
+        longitude (number): The longitude of the location.
+
+    Returns:
+        object: The weather data.
+    """
+    assert isinstance(latitude, (int, float)), "Latitude must be a number"
+    assert isinstance(longitude, (int, float)), "Longitude must be a number"
+
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": "temperature_2m,wind_speed_10m",
+        "hourly": "temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation"
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()  
+
+    return response.json()
+
+agent_general = Agent(name="Assistant", instructions="You are a helpful assistant.")
+agent_weather = Agent(name="Weather Agent", instructions="You can provide the current weather when prompted for weather in a location.",tools=[get_weather_tool])
+triage_agent = Agent(name="Triage Agent", instructions="Route the user to the appropriate agent based on the user's request.", handoffs=[agent_weather])
+
+result = Runner.run_sync(triage_agent, "Which city is currently the coldest? Melbourne, Chicago, Oslo, Dublin, or Shanghai? Will I need an umbrella in this city in the next few days?")
+print(result.final_output)
